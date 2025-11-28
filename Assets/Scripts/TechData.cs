0cd12ae3b7ad69bf7bac1df20137dd6fa61fcd66
BeastlyPizza <shbartnikowski@gmail.com>
2025-11-28 14:59:10 -0800

    added stat block to tech tree description

--- Assets/Scripts/TechData.cs
+++ Assets/Scripts/TechData.cs
@@ -75,7 +75,7 @@ public class TechData : MonoBehaviour
                 "HullOption",
                 HullStatsMutationFunctions.ApplyBasicHull,
                 30,
-                "The basic hull type for your spacecraft - John of all Trades \n\nOne could call this the bricks of the PLF Navy Carrier Regiments. The PLF Navy uses this for all kinds of missions, such as in-atmosphere strikes or basic fleet defense.",
+                "The basic hull type for your spacecraft - John of all Trades \n\nSTATS:\nHealth: 100\nAcceleration Rate : m/s/s\nTurn Rate: degrees/s\nSpecial Ability: None\n\nOne could call this the bricks of the PLF Navy Carrier Regiments. The PLF Navy uses this for all kinds of missions, such as in-atmosphere strikes or basic fleet defense.",
                 "SF-170 Lynchpin"
                 )
         },
@@ -86,7 +86,7 @@ public class TechData : MonoBehaviour
                 "HullOption",
                 HullStatsMutationFunctions.ApplySecondHull,
                 67,
-                "A faster more agile hull with lower base health and more (instability)\n\nIf the Lynchpin is the bricks, then the Swallow is the mortar. The PLF Navy uses this as a basic interdictor fighter, and occasionally they strap fuel pods onto it and use it for escort missions.",
+                "A faster more agile hull with lower base health and more (instability)\n\nSTATS:\nHealth: 85\nAcceleration Rate : m/s/s\nTurn Rate: degrees/s\nSpecial Ability: None\n\nIf the Lynchpin is the bricks, then the Swallow is the mortar. The PLF Navy uses this as a basic interdictor fighter, and occasionally they strap fuel pods onto it and use it for escort missions.",
                 "SI-290 Swallow",
                 new string[] {"BasicHullNode"}   // this some stupid positional argument type shi
                 )
@@ -98,7 +98,7 @@ public class TechData : MonoBehaviour
                 "HullOption",
                 HullStatsMutationFunctions.ApplyScorpionHull,
                 100,
-                "An experimental hull with a the new XD-130 drive system, allowing it to dash short distances.\n\nThis one was a spitball project made by the higher ups after Wraith Industries came up with that dash drive system. Very few prototypes were made because of the price, but it's capable enough that it's very rarely used as a defense fighter. However, to be able to house this equipment and keep its maneuverability it is very fragile, so it's only put into the hands of vetaran pilots.",
+                "An experimental hull with a the new XD-130 drive system, allowing it to dash short distances.\n\nSTATS:\nHealth: 75\nAcceleration Rate : m/s/s\nTurn Rate: degrees/s\nSpecial Ability: Dash\n\nThis one was a spitball project made by the higher ups after Wraith Industries came up with that dash drive system. Very few prototypes were made because of the price, but it's capable enough that it's very rarely used as a defense fighter. However, to be able to house this equipment and keep its maneuverability it is very fragile, so it's only put into the hands of vetaran pilots.",
                 "XSF-347 Scorpion",
                 new string[] {"SecondHullNode"} // same positional argument tysh
             )
@@ -110,7 +110,7 @@ public class TechData : MonoBehaviour
                 "HullOption",
                 HullStatsMutationFunctions.ApplyTrophyHull,
                 125,
-                "A cargo hull with the ability to carry 2 light fighter drones, due to an improved control computer and added space.\n\nAfter the [INSERT CIVIL WAR], the higher ups wanted a way to muster more force to cover more area. After the same competition that resulted in the dash drive concept, Pilen Technologies came up with the Enhanced Capability Trophy Upgrade, or ECTU. This allowed the Trophy to carry 2 light fighter drones, give it the ability to act as an extension of carrier wings.",
+                "A cargo hull with the ability to carry 2 light fighter drones, due to an improved control computer and added space.\n\nSTATS:\nHealth: 110\nAcceleration Rate : m/s/s\nTurn Rate: degrees/s\nSpecial Ability: Drone Carrying\n\nAfter the [INSERT CIVIL WAR], the higher ups wanted a way to muster more force to cover more area. After the same competition that resulted in the dash drive concept, Pilen Technologies came up with the Enhanced Capability Trophy Upgrade, or ECTU. This allowed the Trophy to carry 2 light fighter drones, give it the ability to act as an extension of carrier wings.",
                 "SLC-111C Trophy",
                 new string[] {"SecondHullNode"} // positional arg tysh
             )
@@ -122,7 +122,7 @@ public class TechData : MonoBehaviour
                 "HullOption",
                 HullStatsMutationFunctions.ApplyRavenHull,
                 150,
-                "A fighter equipped with a cloaking pod that allows it to deceive the enemy for a limited period of time.\n\nSome of the scientists at Wraith Industries came up with this cloaking tech after putting a part from their dash drive in the microwave \"for science\". At the same time they were prototyping a multirole capable of operating both in and out of the atmosphere with minimal changes, so the techs decided to merge the tech and make the Raven. The general public, me included, thought that it would be too expensive to go into full production, but the higher ups saw the potential in it and gave Wraith a contract to produce it at the same scale as the Lynchpin.",
+                "A fighter equipped with a cloaking pod that allows it to deceive the enemy for a limited period of time.\n\nSTATS:\nHealth: 100\nAcceleration Rate : m/s/s\nTurn Rate: degrees/s\nSpecial Ability: Cloaking\n\nSome of the scientists at Wraith Industries came up with this cloaking tech after putting a part from their dash drive in the microwave \"for science\". At the same time they were prototyping a multirole capable of operating both in and out of the atmosphere with minimal changes, so the techs decided to merge the tech and make the Raven. The general public, me included, thought that it would be too expensive to go into full production, but the higher ups saw the potential in it and gave Wraith a contract to produce it at the same scale as the Lynchpin.",
                 "SFS-362Raven",
                 new string[] {"TrophyHullNode", "ScorpionHullNode"} // positional arg tysh
             )
