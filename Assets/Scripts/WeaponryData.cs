c61636a51ad0188c8d40428507d517346cbf3448
BeastlyPizza <shbartnikowski@gmail.com>
2025-11-29 10:11:39 -0800

    forgot a }

--- Assets/Scripts/WeaponryData.cs
+++ Assets/Scripts/WeaponryData.cs
@@ -40,7 +40,7 @@ namespace Sebastian
             public static float GetRandomError(float maxError)
             {
                 double random1tominus1 = (_random.NextDouble() * 2) - 1;
-                return (float) random1tominus1 * maxError;
+                return (float)random1tominus1 * maxError;
             }
             private static float GetNormalDistributedError(float maxError)
             {
@@ -65,7 +65,7 @@ namespace Sebastian
 
                 float randomOffsetFactor = GetNormalDistributedError(maxDegreeError);
 
-                float trueRotation = ParentRotation + (float) randomOffsetFactor;   
+                float trueRotation = ParentRotation + (float)randomOffsetFactor;
 
                 Vector2 VelocityFromMuzzle = new Vector2(Mathf.Cos(trueRotation * Mathf.Deg2Rad), Mathf.Sin(trueRotation * Mathf.Deg2Rad)) * MuzzleVelo;
 
@@ -77,7 +77,7 @@ namespace Sebastian
                 OrphanBulletScript.velocity = newVeloVector;
             }
 
-        public static Dictionary<int, Weapon> WeaponDict = new()
+            public static Dictionary<int, Weapon> WeaponDict = new()
         {
             {
                 //27mm
@@ -88,8 +88,9 @@ namespace Sebastian
                 //25mm rotary
                 2,
                 new Weapon(1500, WeaponryActions.BasicBulletSpawnAction, 20, 10)
-                
+
             }
         };
+        }
     }
 }
