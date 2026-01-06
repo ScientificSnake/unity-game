dad57b218cacb5d3bced9c0e88712cc4154d7974
ScientificSnake <andrew.segerlind@gmail.com>
2026-01-06 14:44:45 -0800

    Started work on guided missle with rb physics rotation

---
+++ Assets/BaseMissle.cs
@@ -0,0 +1,111 @@
+using System.Runtime.CompilerServices;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class BaseMissle : ProjectileTemplate
+{
+    private Sprite spCold;
+    private Sprite spTurnRight;
+    private Sprite spTurnLeft;
+    private Sprite spStraight;
+
+    private SpriteRenderer spRenderer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        SpriteRenderer spRenderer = GetComponent<SpriteRenderer>();
+        rb.angularDamping = 0.25f;
+    }
+
+    protected enum ThrustState
+    {
+        NoLock,
+        TurnRight,
+        TurnLeft,
+        Straight,
+        Cold
+    }
+
+    protected ThrustState thisThrustState;
+
+    #region Sprite changes
+
+    public enum MissleSpriteEnum
+    {
+        Cold,
+        TurnLeft,
+        TurnRight,
+        Straight
+    }
+    public void ChangeSprite(MissleSpriteEnum targetSprite, Sprite newsprite)
+    {
+        switch (targetSprite)
+        {
+            case MissleSpriteEnum.Cold:
+                spCold = newsprite;
+                break;
+            case MissleSpriteEnum.TurnLeft:
+                spTurnLeft = newsprite;
+                break;
+            case MissleSpriteEnum.TurnRight:
+                spTurnRight = newsprite;
+                break;
+            case MissleSpriteEnum:
+                spStraight = newsprite;
+                break;
+        }
+    }
+
+    #endregion
+
+    private float Fuel;
+
+    private Rigidbody2D PlayerLock;
+
+    protected void FixedUpdate()
+    {
+        Vector2 directionToPlayer = PlayerLock.position - (Vector2) transform.position;
+
+        GetThrustState(directionToPlayer.DirectionAngle());
+        UseThrustState();
+
+    }
+
+    private void GetThrustState(float targetAngle)
+    {
+        float deltaAngle = Mathf.DeltaAngle(targetAngle, rb.transform.eulerAngles.z);
+
+        if (deltaAngle == 0)
+        {
+            thisThrustState = ThrustState.Straight;
+        }
+        else if (deltaAngle > 0)
+        {
+            thisThrustState = ThrustState.TurnRight;
+        }
+        else if (deltaAngle < 0)
+        {
+            thisThrustState = ThrustState.TurnLeft;
+        }
+    }
+
+    private void UseThrustState()
+    {
+        switch (thisThrustState)
+        {
+            case ThrustState.Straight:
+                spRenderer.sprite = spStraight;
+                break;
+            case ThrustState.TurnRight:
+                spRenderer.sprite = spTurnRight;
+                break;
+            case ThrustState.TurnLeft:
+                spRenderer.sprite = spTurnLeft;
+                break;
+            case ThrustState.Cold:
+                spRenderer.sprite = spCold;
+                break;
+        }
+    }
+}
